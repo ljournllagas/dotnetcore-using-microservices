7652f54f6918c71d38282be31526d0bb79ff0710
Ljourn Llagas <llagas.ljournkevin@bdo.com.ph>
2023-11-27 17:30:10 +0800

    Added project references and set-up Program/Startup.cs

---
+++ EShopping/Catalog/Catalog.API/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Catalog.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => 
+            { 
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
